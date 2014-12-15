229ed934b749c55ecb3997455feae2629506da84
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2014-12-15 17:39:47 -0500

    pg numbers

--- src/TableWriter.py
+++ src/TableWriter.py
@@ -3,21 +3,28 @@ from Table import Table
 import errno
 
 class TableWriter:
-    def __init__(self, table, outputdir, rowsPerPage = 20):
+    def __init__(self, table, outputdir, rowsPerPage = 20, pgListBreak = 20):
         self.outputdir = outputdir
         self.rowsPerPage = rowsPerPage
         self.table = table
+        self.pgListBreak = pgListBreak
     def write(self):
         self.mkdir_p(self.outputdir)
         nRows = self.table.countRows()
-        counter = 1
+        pgCounter = 1
         for i in range(0, nRows, self.rowsPerPage):
             rowsSubset = self.table.rows[i : i + self.rowsPerPage]
             t = Table(self.table.headerRows + rowsSubset)
-            f = open(os.path.join(self.outputdir, str(counter) + '.html'), 'w')
+            f = open(os.path.join(self.outputdir, str(pgCounter) + '.html'), 'w')
+            pgLinks = self.getPageLinks(nRows / self.rowsPerPage + 1, 
+                    pgCounter, self.pgListBreak)
+            
+            f.write(pgLinks)
             f.write(t.getHTML())
+            f.write(pgLinks)
+            
             f.close()
-            counter += 1
+            pgCounter += 1
     @staticmethod
     def mkdir_p(path):
         try:
@@ -26,4 +33,15 @@ class TableWriter:
             if exc.errno == errno.EEXIST and os.path.isdir(path):
                 pass
             else: raise
+    @staticmethod
+    def getPageLinks(nPages, curPage, pgListBreak):
+        links = ""
+        for i in range(1, nPages + 1):
+            if not i == curPage:
+                links += '<a href="' + str(i) + '.html">' + str(i) + '</a>&nbsp'
+            else:
+                links += str(i) + '&nbsp'
+            if (i % pgListBreak == 0):
+                links += '<br />'
+        return links
 
